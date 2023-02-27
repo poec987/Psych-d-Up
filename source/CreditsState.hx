a153afa7d8024aa6da67b4088bcc8eda324df103
poec987 <poedev08@outlook.com>
2023-02-27 15:54:24 -0600

    Creds
    susy

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,6 +81,10 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+			['Psych\'d Up Team'],
+			['PoeDev',		'poedev',		'Main Programmer of Psych\'d Up',								'https://twitter.com/Shadow_Mario_',	'0000FF'],
+			['Didgie',		'didgie',		'Co-Programmer of Psych\'d Up',								    'https://twitter.com/Shadow_Mario_',	'FFFFFF'],
+			[''],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',								'https://twitter.com/Shadow_Mario_',	'444444'],
 			['RiverOaken',			'river',			'Main Artist/Animator of Psych Engine',							'https://twitter.com/RiverOaken',		'B42F71'],
