12664b1864c24789521bab2908570be6366d1a63
Didgie <bradyboy1120@gmail.com>
2023-03-11 14:58:12 -0800

    winning icons

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3191,13 +3191,17 @@ class PlayState extends MusicBeatState
 
 		if (healthBar.percent < 20)
 			iconP1.animation.curAnim.curFrame = 1;
-		else
+		if (healthBar.percent > 20)
 			iconP1.animation.curAnim.curFrame = 0;
-
+		if (healthBar.percent > 80)
+			iconP1.animation.curAnim.curFrame = 2;
+		
+		if (healthBar.percent < 20)
+			iconP2.animation.curAnim.curFrame = 2;
+		if (healthBar.percent > 20)
+			iconP2.animation.curAnim.curFrame = 0;
 		if (healthBar.percent > 80)
 			iconP2.animation.curAnim.curFrame = 1;
-		else
-			iconP2.animation.curAnim.curFrame = 0;
 
 		if (FlxG.keys.anyJustPressed(debugKeysCharacter) && !endingSong && !inCutscene) {
 			persistentUpdate = false;
